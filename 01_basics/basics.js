b5a108286ee5cfd465d684a66786a71dcbe6b168
Farooq Behan <56262805+Farooquebehan@users.noreply.github.com>
2023-08-25 07:45:43 +0000

    container config

---
+++ 01_basics/basics.js
@@ -0,0 +1,44 @@
+
+
+//for(var index = 0 ; index <= 20 ; index++){
+    //     if(index == 5){
+    //         console.log(`Dected 5`);
+    //         continue
+    //     }
+    //     console.log(`value of i is ${index}`);
+    // }
+    
+    
+    
+    
+    // let myArry = ["farook" , "Muskan" , "Are" , "Husband wife"]
+    // console.log(myArry.length)
+    // for(let index = 0 ; index < myArry.length; index++){
+    //     const element = myArry[index];
+    //     console.log(element);
+    // }
+    
+    // for(var i = 0 ; i <= 10; i++){
+    //     console.log(`outer loop ${i}`);
+    //     for(let j = 0; j<=10; j++){
+    //         // console.log(`inner loop ${j} and inner loop ${i}`);
+    //     console.log(i + "*" + j + "=" + i * j)
+    //     }
+    // }
+    
+    
+    
+    // for(var index = 0; index <= 10; index++  ){
+    //     const element = index;
+    //     if(element == 5){
+    //         console.log("5 is the best number")
+    //     }
+    //     console.log(element);
+    // }
+
+
+
+// for(var i = 0 ; i <= 10; i++){
+//     const element = i;
+//     console.log(i)
+// }
